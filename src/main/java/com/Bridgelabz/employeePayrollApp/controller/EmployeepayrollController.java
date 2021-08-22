044859ee3c6fd4bd92bed960b4e0f02f166e673c
Anand27git <anandkumar256k@gmail.com>
2021-08-22 15:44:07 +0530

    EmpValidation-ExceptionUC-1

--- src/main/java/com/Bridgelabz/employeePayrollApp/controller/EmployeepayrollController.java
+++ src/main/java/com/Bridgelabz/employeePayrollApp/controller/EmployeepayrollController.java
@@ -8,7 +8,7 @@ import  org.springframework.beans.factory.annotation.Autowired;
 import  org.springframework.http.HttpStatus;
 import  org.springframework.http.ResponseEntity;
 import  org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -37,7 +37,7 @@ public class EmployeepayrollController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
+    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData empData = null;
         empData = employeePayrollService.createEmployeePayrollData(empPayrollDTO);
         ResponseDTO respDTO = new ResponseDTO("Created Employee Payroll Data Successfully", empData);
@@ -45,7 +45,7 @@ public class EmployeepayrollController {
     }
 
     @PutMapping("/update/{empId}")
-    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId, @RequestBody EmployeePayrollDTO empPayrollDTO) {
+    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId, @Valid @RequestBody EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData empData = null;
         empData = employeePayrollService.updateEmployeePayrollData(empId, empPayrollDTO);
         ResponseDTO respDTO = new ResponseDTO("Updated Employee Payroll Data Successfully", empData);
